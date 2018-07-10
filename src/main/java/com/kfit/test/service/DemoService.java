9bfe1d530f31c8b0ed33a9abfb560af99730c406
kzrs55 <569726429@qq.com>
2018-07-10 08:44:55 +0800

    three

--- src/main/java/com/kfit/test/service/DemoService.java
+++ src/main/java/com/kfit/test/service/DemoService.java
@@ -2,6 +2,7 @@ package com.kfit.test.service;
 
 import com.kfit.dao.DemoRepository;
 import com.kfit.test.bean.Demo;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
@@ -9,6 +10,7 @@ import javax.transaction.Transactional;
 /**
  * Created by zjutK on 2018/7/8.
  */
+@Service
 public class DemoService {
     @Resource
     private DemoRepository demoRepository;
